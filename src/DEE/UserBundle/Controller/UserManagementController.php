044c5e778e782d63721fbb2f675ea79e24d77ab1
Gauthier Delphine <keninette.bloggeusedeluxe@gmail.com>
2016-06-01 19:30:11 +0200

    layout.backend terminé et nav ok

--- src/DEE/UserBundle/Controller/UserManagementController.php
+++ src/DEE/UserBundle/Controller/UserManagementController.php
@@ -13,9 +13,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class UserManagementController extends Controller {
     
     public function indexAction() {
+        return $this->render('DEEUserBundle:UserManagement:index.html.twig');
+    }
+    
+    public function usersAction() {
         $userManager = $this->get('fos_user.user_manager') ;
         $users = $userManager->findUsers();
         
-        return $this->render('DEEUserBundle:UserManagement:index.html.twig', array('users' => $users));
+        return $this->render('DEEUserBundle:UserManagement:users.html.twig', array('users' => $users));
     }
 }
