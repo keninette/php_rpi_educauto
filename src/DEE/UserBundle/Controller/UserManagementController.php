0ead046c82538f59fc365ce00ad276d2c2766bc0
Gauthier Delphine <keninette.bloggeusedeluxe@gmail.com>
2016-06-02 07:50:01 +0200

    début activation/désactivation utilisateur

--- src/DEE/UserBundle/Controller/UserManagementController.php
+++ src/DEE/UserBundle/Controller/UserManagementController.php
@@ -3,7 +3,9 @@
 // src/DEE/UserBundle/Controller/UserManagementController.php
 
 namespace DEE\UserBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
  * Description of UserManagementController
@@ -16,6 +18,9 @@ class UserManagementController extends Controller {
         return $this->render('DEEUserBundle:UserManagement:index.html.twig');
     }
     
+    /**
+   * @Security("has_role('ROLE_SUPER_ADMIN')")
+   */
     public function usersAction() {
         $userManager = $this->get('fos_user.user_manager') ;
         $users = $userManager->findUsers();
@@ -23,11 +28,26 @@ class UserManagementController extends Controller {
         return $this->render('DEEUserBundle:UserManagement:users.html.twig', array('users' => $users));
     }
     
-    public function deactivateAction($id) {
+    /**
+   * @Security("has_role('ROLE_SUPER_ADMIN')")
+   */
+    public function activateAction($id) {
         var_dump($id);
-        $userManager = $this->get('fos_user.user_manager') ;
-        $users = $userManager->findUsers();
         
-        return $this->render('DEEUserBundle:UserManagement:users.html.twig', array('users' => $users));
+        // Get user
+        $userManager = $this->get('fos_user.user_manager') ;
+        $user = $userManager->findUserBy(array('id' => $id));
+        
+        // Deactivate user & add flashbag
+        if ($user->getEnabled()) {
+             $user->setEnabled(false);
+             $this->getSession()->getFlashBag()->add('notice','L\'utilisateur a bien été désactivé');
+        } else {
+            $user->setEnabled(true);
+            $this->getSession()->getFlashBag()->add('notice','L\'utilisateur a bien été activé');
+        }  
+        $userManager->update($user);
+        
+        return new JsonResponse(array('success' => true));
     }
 }
