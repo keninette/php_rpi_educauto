e4ab88a5eed2fcf2bb48263e07cb55ebb104471f
kbj <keninette.bloggeusedeluxe@gmail.com>
2016-06-14 17:23:07 +0200

    debut ajout génération nom aléatoire

--- src/DEE/FtpBundle/Entity/FtpFile.php
+++ src/DEE/FtpBundle/Entity/FtpFile.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
  * @ORM\Entity(repositoryClass="DEE\FtpBundle\Repository\FtpFileRepository")
  */
 class FtpFile {
-
+    private static $CST_NAME_LENGTH = 15;
     /**
      * @var int
      *
@@ -119,6 +119,29 @@ class FtpFile {
      */
     public function setFile(UploadedFile $file = null) {
         $this->file = $file;
+    }
+    
+    /**
+     * Creates a random password composed with alphanumeric and special characters
+     * Set this password to the current user's password
+     * @return none
+     * 
+     * ORM\PrePersist
+     */
+    public function createRandomName() {
+        // Creating an array with alphanumeric
+        $chars          = explode(',','a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z'
+                                        .',A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+                                        .',0,1,2,3,4,5,6,7,8,9');
+        $maxRange       = sizeof($chars) -1;
+        $string         = '';
+        
+        // Add a random char to string
+        for($charCounter = 0; $charCounter < self::CST_NAME_LENGTH; $charCounter++) {
+            $string .= $chars[rand(0,$maxRange)];
+        } 
+        
+        return $string;
     }
 
 }
